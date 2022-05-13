f7fcf9c4f5418923866296cc565312d8ed88e404
khkg12 <78714947+khkg12@users.noreply.github.com>
2022-05-13 12:47:31 +0900

    날씨 이모티콘 추가

--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,29 @@
-const API_KEY = "b44684465af49eebcb5d6868c2f7051f"
+const API_KEY = "b44684465af49eebcb5d6868c2f7051f";
 
-function onGeoOk(position){
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:first-child").nextElementSibling; // = document.querySelector("#weather span:last-child");
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-        city.innerText = data.name;
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector(
+        "#weather span:first-child"
+      ).nextElementSibling; // = document.querySelector("#weather span:last-child");
+      const icon = document.querySelector(".icon");
+      const weatherIcon = data.weather[0].icon; // 아래링크로 이모티콘을 가져오기 위한 json 내 icon 코드 가져오기
+      weather.innerText = `WEATHER : ${data.weather[0].main} / TEMP : ${data.main.temp}°C`;
+      city.innerText = `/ LOCATION : ${data.name}`;
+      icon.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+      ); // setAttribute를 통해 icon의 src 속성값을 변경, 다음의 링크는 icon이미지를 불러오는 api링크
     });
-} 
+}
 
-function onGeoError(){
-    alert("Can't find you,. No weather for you.")
+function onGeoError() {
+  alert("Can't find you,. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-
